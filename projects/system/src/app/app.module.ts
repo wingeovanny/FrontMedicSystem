8110b0f8d1fc8d394d289ce2e53996665d75257c
Geovanny Bucay <ebucayle@pichincha.com>
2023-07-29 18:58:15 -0500

    wip: add components

--- projects/system/src/app/app.module.ts
+++ projects/system/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LiskTaskComponent } from './components/lisk-task/lisk-task.component';
+import { ResponsibleComponent } from './components/responsible/responsible.component';
+import { ShowTimeComponent } from './components/show-time/show-time.component';
+import { FormTaskComponent } from './components/form-task/form-task.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LiskTaskComponent, ResponsibleComponent, ShowTimeComponent, FormTaskComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
