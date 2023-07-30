94d0752bb140d88911665cabe1ee85eb64a40b27
Geovanny Bucay <ebucayle@pichincha.com>
2023-07-29 19:27:47 -0500

    fin clase 01

--- projects/system/src/app/components/form-task/form-task.component.ts
+++ projects/system/src/app/components/form-task/form-task.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'amb-form-task',
   templateUrl: './form-task.component.html',
-  styleUrls: ['./form-task.component.css']
+  styleUrls: ['./form-task.component.css'],
 })
 export class FormTaskComponent {
-
+  descriptionTask!: string;
+  save() {
+    console.log('button clicked');
+  }
+  captureDescription(evt: any) {
+    this.descriptionTask = evt.target.value;
+  }
 }
